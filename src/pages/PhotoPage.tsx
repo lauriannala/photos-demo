6a880c84c8390ba3e0832e54f40046476ed14f6a
Lauri Annala <lauri.annala@protonmail.com>
2024-05-09 15:36:09 +0300

    Back-button navigates back in browser history

--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -1,10 +1,10 @@
 import { Container, Typography, Box, Stack, Button } from '@mui/material'
-import { Link, useParams } from 'react-router-dom'
-import { routes } from '../constants/routes'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Photo } from '../features/photo/Photo'
 
 const PhotoPage = () => {
     const { id } = useParams<{ id: string }>()
+    const navigate = useNavigate()
 
     return (
         <>
@@ -12,7 +12,7 @@ const PhotoPage = () => {
                 <Stack spacing={3} sx={{ mt: 3 }}>
                     <Photo id={id as string} />
                     <Box my={2}>
-                        <Button component={Link} to={routes.photos} variant="contained">
+                        <Button onClick={() => navigate(-1)} variant="contained">
                             <Typography variant="body1">
                                 Back to list
                             </Typography>
